0ba16f39b51e90ffd9665ae3879cc1b32e5682a2
Eduardo C <eduardocezarinossp@gmail.com>
2018-09-24 21:14:47 -0300

    a

--- cadastro/admin.py
+++ cadastro/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Conta
+admin.site.register(Conta)
+from .models import Item
+admin.site.register(Item)
 # Register your models here.
--- cadastro/models.py
+++ cadastro/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
+class Conta(models.Model) :
+    descricao = models.CharField(max_length=50)
+    valor = models.DecimalField
+    item = models.ForeignKey('Item', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.descricao
+
+class Item(models.Model) :
+    nome = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
