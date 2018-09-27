0605b30a1d3ead1d4bf527491e3b279516cfcfb5
Eduardo C <eduardocezarinossp@gmail.com>
2018-09-27 19:32:48 -0300

    2

--- cadastro/models.py
+++ cadastro/models.py
@@ -6,8 +6,9 @@ from django.utils import timezone
 class Conta(models.Model) :
     descricao = models.CharField(max_length=50)
     valor = models.DecimalField
+    data = models.CharField(max_length=50)
     item = models.ForeignKey('Item', on_delete=models.CASCADE)
-
+    responsavel = models.CharField(max_length=50)
     def __str__(self):
         return self.descricao
 
