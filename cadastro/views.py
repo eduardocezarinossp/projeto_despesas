7a839a86c44c5e4696b6c061344918af8758e929
Eduardo C <eduardocezarinossp@gmail.com>
2018-09-27 19:56:19 -0300

    3

--- cadastro/views.py
+++ cadastro/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+def ola_mundo(request):
+    return render(request, 'ola_mundo.html', {})
+
+def segundapagina(request):
+    return render(request, 'segundapagina.html', {})
 # Create your views here.
